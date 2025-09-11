98400ce85f9df78a217c77ca2ff1da8aa850b1b9
LCuadrado-Morillas4 <lois.cuadrado@gmail.com>
2025-09-11 20:15:41 +0200

    View "accueil"
    View "accueil" migrated from bootstrap 3 to 5.4

--- src/Vues/v_accueil.php
+++ src/Vues/v_accueil.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Vue Accueil
  *
@@ -14,38 +13,41 @@
  * @version   GIT: <0>
  * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
  */
-
 ?>
 <div class="alert alert-warning" role="alert"><strong>Rappel : </strong>Vos frais sont à déclarer au plus tard le dernier jour du mois 
-	et vos factures acquittées doivent être arrivées aux services comptables au plus tard le 10 du mois suivant la saisie.
-	Les éléments reçus après le 10 seront reportés sur le mois suivant.
+    et vos factures acquittées doivent être arrivées aux services comptables au plus tard le 10 du mois suivant la saisie.
+    Les éléments reçus après le 10 seront reportés sur le mois suivant.
 </div>
 <div id="accueil">
     <h2>
-        Gestion des frais<small> - Visiteur : 
-            <?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></small>
+        Gestion des frais 
+        <small class="text-muted"> - Visiteur : 
+<?= $_SESSION['prenom'] . ' ' . $_SESSION['nom'] ?></small>
     </h2>
 </div>
+
 <div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <h3 class="panel-title">
-                    <span class="glyphicon glyphicon-bookmark"></span>
+    <div class="col-12">
+        <div class="card border-primary">
+            <div class="card-header bg-primary text-white">
+                <h3 class="card-title mb-0">
+                    <i class="bi bi-bookmark-fill"></i>
                     Navigation
                 </h3>
             </div>
-            <div class="panel-body">
+            <div class="card-body">
                 <div class="row">
-                    <div class="col-xs-12 col-md-12">
+                    <div class="col-12">
                         <a href="index.php?uc=gererFrais&action=saisirFrais"
                            class="btn btn-success btn-lg" role="button">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                            <br>Renseigner la fiche de frais</a>
+                            <i class="bi bi-pencil-fill"></i>
+                            <br>Renseigner la fiche de frais
+                        </a>
                         <a href="index.php?uc=etatFrais&action=selectionnerMois"
                            class="btn btn-primary btn-lg" role="button">
-                            <span class="glyphicon glyphicon-list-alt"></span>
-                            <br>Afficher mes fiches de frais</a>
+                            <i class="bi bi-list-ul"></i>
+                            <br>Afficher mes fiches de frais
+                        </a>
                     </div>
                 </div>
             </div>
